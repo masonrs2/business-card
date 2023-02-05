fb4027ed83612db8a33c4255b3ecbfd3a841805a
Mason <masonsepulveda@gmail.com>
2023-02-04 21:56:38 -0600

    installed additonal modules

--- utils/supabase-server.js
+++ utils/supabase-server.js
@@ -1,5 +1,5 @@
 import { headers, cookies } from 'next/headers'
-import { createServerComponentSupabaseClient } from 'supabase/auth-helpers-nextjs'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
 export const createClient = () =>
   createServerComponentSupabaseClient({
