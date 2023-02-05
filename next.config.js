824c9618a70fcea2cfa6dd80751a4de2cdafe6de
Mason <masonsepulveda@gmail.com>
2023-02-04 21:42:52 -0600

    installed additonal modules

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  webpack(config, { nextRuntime }) { 
+    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+    if (typeof nextRuntime === "undefined") {
+      const { IgnorePlugin } = require("webpack");
+      const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+      config.plugins.push(ignoreFs);
+    }
+
+    return config;
+  },
   
   
 }
