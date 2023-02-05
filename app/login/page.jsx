cb8f27bd5e8a6f7e5b3d5b1b7dc8b698aeea30ee
Mason <masonsepulveda@gmail.com>
2023-02-05 17:49:39 -0600

    temp redirect error fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useSupabase } from '../../components/supabase-provider'
 import { useRouter } from 'next/navigation'
+
 const login = () => {
   const { supabase, session } = useSupabase()
   const router = useRouter()
@@ -15,15 +16,27 @@ const login = () => {
     if(error) throw error
   }
 
+  async function signOut() {
+    const { error } = await supabase.auth.signOut()
+
+    if(error) throw error;
+    
+}
+
   return (
-    <div className="flex h-[calc(100vh-4rem)] flex-col items-center justify-center bg-gradient-to-br from-rose-500 to bg-purple-600 ">
-          {!session && (
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-br from-rose-500 to bg-purple-600 ">
+          {!session ? (
             <button 
-              onClick={session ? () => signOut() : () => signInWithGoogle()}
+              onClick={session ? null : () => signInWithGoogle()}
               className="rounded-full bg-black/10 px-10 py-3 font-semibold active:bg-black/40 text-gray-100 no-underline transition hover:bg-black/2-">
                 Sign in with Google
               </button>
-          )}
+          )
+          : 
+            router.push('/')
+          
+        }
+
 
       </div>
   )
